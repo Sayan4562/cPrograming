9d02f79197e8170d764e078792cc0c997e068c46
Sayan4562 <97144518+Sayan4562@users.noreply.github.com>
2023-08-29 08:17:17 -0700

    Add files via upload

---
+++ addNumber.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    printf("Enter a number\n");
+    scanf("%d",&b);   
+    printf("result %d", a+b);
+    return 0;
+
+
+}
---
+++ multipal2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a ,b=1;
+    for (a=1;a<=5;a++){
+        b=b*a;
+        printf("%d\n",b);
+
+    }
+    return 0;
+}
